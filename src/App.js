fae61041586e965a1b7b2815682bf114e41c2a96
bbissing <bbissing101@gmail.com>
2023-01-23 03:22:51 -0600

    additional changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 import HelloWorld from './components/helloWorld';
 
 function App() {
 
-  return (
-    <HelloWorld />
-  )
   // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
+  //   <HelloWorld />
+  // )
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
