565a89f11c5e53b224a948f7d6cbf19516a21f86
bbissing <bbissing101@gmail.com>
2023-01-22 21:56:37 -0600

    changed port from 3000 to 8080

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const port = 3000;
+const port = 8080;
 const app = express();
 // eslint-disable-next-line object-curly-newline
 app.use(bodyParser.json());
