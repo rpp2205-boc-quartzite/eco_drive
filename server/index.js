c5dd426a83d798c46dd998e6c5cc7dc5892b7ea3
bbissing <bbissing101@gmail.com>
2023-01-23 02:20:45 -0600

    additional changes hello/goodbye examples

--- server/index.js
+++ server/index.js
@@ -1,22 +1,13 @@
-/* eslint-disable import/no-extraneous-dependencies */
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
-
-const port = 8080;
+const goodbye = require('./routes/goodbye.js');
+const port = 3001;
 const app = express();
-// eslint-disable-next-line object-curly-newline
+
 app.use(bodyParser.json());
-// app.use('/', express.static(path.join(__dirname, '../index.html')));
-// app.use('/', express.static(path.join('public')));
+app.use('/goodbye', goodbye);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
-});
 
-const server = app.listen(port, () => {
-  // eslint-disable-next-line no-console
+app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
-// eslint-disable-next-line eol-last
-module.exports = server;
