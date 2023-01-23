038b1b9ccc79775b0a83bd7344501cc661e309db
bbissing <bbissing101@gmail.com>
2023-01-22 19:55:14 -0600

    set up create-react-app

--- config.example.js
+++
@@ -1,5 +0,0 @@
-// copy this file and fill in your environment variables
-// then change the file name to '.env'
-
-// # database address
-// DATABASE_CONNECTION_ADDRESS= 'mongodb://localhost/greenEarth'
