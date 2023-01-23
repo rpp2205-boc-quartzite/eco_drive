271d655b9cade284a5f72ee90e6eb0d2e02b65fb
bbissing <bbissing101@gmail.com>
2023-01-22 21:51:50 -0600

    create react app

---
+++ config.example.js
@@ -0,0 +1,5 @@
+// copy this file and fill in your environment variables
+// then change the file name to '.env'
+
+// # database address
+// DATABASE_CONNECTION_ADDRESS= 'mongodb://localhost/greenEarth'
---
+++ server/index.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const port = 3000;
+const app = express();
+// eslint-disable-next-line object-curly-newline
+app.use(bodyParser.json());
+// app.use('/', express.static(path.join(__dirname, '../index.html')));
+// app.use('/', express.static(path.join('public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+const server = app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`listening on port ${port}`);
+});
+// eslint-disable-next-line eol-last
+module.exports = server;
