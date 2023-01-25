78b254c198e6d9d53d4c4da090584f59a3570f19
Blake Crawford <blake.crawford@outlook.com>
2023-01-24 20:29:23 -0800

    added config  headers to handle CORS errors

--- server/index.js
+++ server/index.js
@@ -16,6 +16,18 @@ const db = require('../database/index.js');
 // db controllers
 const User = require('../database/controllers/user.js');
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
 
 // ----  Routes ---- //
 
