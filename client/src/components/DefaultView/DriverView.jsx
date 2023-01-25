63d6933967e9dd9d68ecdbdeeb0db6771d6af58c
netbek932 <rjeren22@gmail.com>
2023-01-25 01:41:12 -0500

    Basic Driver view page setup, no routing

--- client/src/components/DefaultView/DriverView.jsx
+++ client/src/components/DefaultView/DriverView.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Autocomplete from "react-google-autocomplete";
+// import TimePicker from 'react-time-picker';
 
 class DriverView extends React.Component {
   constructor(props) {
@@ -9,17 +10,51 @@ class DriverView extends React.Component {
       authenticatedUserID: '', // The successfully authenticated user's ID, not sure what Blake will call this prop yet
       userName: '',
       start_address: '',
+      start_lat: '',
+      start_lng: '',
       end_address: '',
       time: '',
       total_seats: '',
+      end_lat: '',
+      end_lng: '',
       avatar: ''
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit() {
+    console.log(this.state)
+  }
+
+  handleChange (e, field) {
+    if (field === 'start_address') {
+      var lat = e.geometry.location.lat();
+      var lng = e.geometry.location.lng();
+      this.setState({
+        start_address: e.formatted_address,
+        start_lat: lat,
+        start_lng: lng
+      })
+    } else if (field === 'end_address') {
+      lat = e.geometry.location.lat();
+      lng = e.geometry.location.lng();
+      this.setState({
+        end_address: e.formatted_address,
+        end_lat: lat,
+        end_lng: lng
+      })
+    } else if (field === 'start_time') {
+        this.setState({ time: e.target.value })
+    } else if (field === 'total_seats') {
+      this.setState({ total_seats: e.target.value})
+    }
   }
 
   render () {
     return (
       <div>
-        <div>Switch to Rider View</div>
+        <small>Switch to Rider View</small>
         <h2>Welcome [name],</h2>
         <h3>Find your nearest riders</h3>
           <form>
@@ -29,6 +64,7 @@ class DriverView extends React.Component {
               style={{ width: "90%" }}
               placeholder="Starting point"
               onPlaceSelected={(place) => {
+                this.handleChange(place, 'start_address')
                 console.log(place);
               }}
               options={{
@@ -41,6 +77,7 @@ class DriverView extends React.Component {
               style={{ width: "90%" }}
               placeholder="Destination"
               onPlaceSelected={(place) => {
+                this.handleChange(place, 'end_address')
                 console.log(place);
               }}
               options={{
@@ -48,15 +85,26 @@ class DriverView extends React.Component {
                 componentRestrictions: { country: "us" },
               }}
             />
-            <input type="text" name="StartTime" style={{ width: "90%" }} placeholder="Start time"/> <br/>
-            <input type="text" name="AvailableSeats" style={{ width: "90%" }} placeholder="Available seats"/> <br/>
+            {/* <TimePicker onChange={(e) => this.handleChange(e, 'start_time')} value={'10:00'} /> */}
+            <input type="text" name="StartTime" style={{ width: "90%" }} placeholder="Start time" onChange={(e) => this.handleChange(e, 'start_time')}/> <br/>
+            <input type="text" name="AvailableSeats" style={{ width: "90%" }} placeholder="Available seats" onChange={(e) => this.handleChange(e, 'total_seats')}/> <br/>
             <input type="radio" value="SaveDefaultRoute"  name="default"/> Set as default route <br/>
-            <button type="Submit" className="findRiders">Find riders</button>
+            <button type="Submit" className="findRiders" onClick={this.handleSubmit}>Find riders</button>
           </div>
           </form>
 
-
-
+        <div>
+          _____________________________________________ <br/>
+          Ongoing Trip
+        </div>
+        <div>
+          _____________________________________________ <br/>
+          Upcoming Trip
+        </div>
+        <div>
+          _____________________________________________ <br/>
+          Default route
+        </div>
       </div>
     )
   }
