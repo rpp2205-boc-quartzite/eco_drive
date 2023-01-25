16b65c1ad8145ca0b3e017df4cb86fabf5351b64
Blake Crawford <blake.crawford@outlook.com>
2023-01-24 17:46:54 -0800

    updated User path in authentication controller, verifed routes successful

--- database/controllers/authentication.js
+++ database/controllers/authentication.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const User = require('../models/user.js').User;
 
 module.exports = {
   register: (req, res) => {
