087ae4d446b2d96fa0bb719447c1d78413e66913
Chelsea Pae <chelseapae@gmail.com>
2023-01-25 21:00:48 -0800

    Add icons

--- client/src/components/UserProfile/DriverProfile.jsx
+++ client/src/components/UserProfile/DriverProfile.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { AiFillHome } from 'react-icons/Ai';
+import { MdLogout } from 'react-icons/Md';
+import { HiOutlineRefresh } from 'react-icons/Hi';
+import { FaPen } from 'react-icons/Fa';
 
 class DriverProfile extends React.Component {
   constructor(props) {
@@ -19,7 +23,12 @@ class DriverProfile extends React.Component {
   render () {
     return (
       <div>
-
+      Rider <HiOutlineRefresh/>
+      <AiFillHome/>
+      <MdLogout />
+      Update Profile <FaPen
+          size="10px"
+          color="green" />
       </div>
     )
   }
