459415401b823991d98598537ac329116f185208
bbissing <bbissing101@gmail.com>
2023-01-28 00:35:31 -0600

    needed to make a few edits to package.json

--- client/src/components/RatingsReviews/StarRating.jsx
+++ client/src/components/RatingsReviews/StarRating.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { FaStar } from "react-icons/fa";
+import { FaStar } from "react-icons/Fa";
 
 const StarRating = ({click}) => {
   const [rating, setRating] = useState(null);
