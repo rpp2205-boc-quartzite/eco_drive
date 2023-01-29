323b5798857453ab1bec5ce9af25d12e6e992ab5
Huan Tran <huanductran98@gmail.com>
2023-01-29 02:09:02 -0800

    Complete passing ongoing route data to rider list component after booking driver

--- client/src/components/DriverList/DriverList.jsx
+++ client/src/components/DriverList/DriverList.jsx
@@ -1,35 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './driver-list-style.css'
+import { useLocation } from 'react-router-dom'
 
 import DriverCard from './DriverCard.jsx'
 
 const DriverList = (props) => {
 
   const [drivers, setDrivers] = useState([]);
-  const [bookedDriver, setBookedDriver] = useState({})
+
+  const info = useLocation()
+  const {route} = info.state
 
   const findDrivers = () => {
-    // const rider = {
-    //   id: props.userId,
-    //   start_address: props.start_address,
-    //   start_lat: props.start_lat,
-    //   start_lng: props.start_lng,
-    //   end_address: props.end_address,
-    //   end_lat: props.end_lat,
-    //   end_lng: props.end_lng,
-    //   time: props.time,
-    // }
+    console.log(route)
     const rider = {
-      id: "63d36e02cd478f26557c4a36",
-      start_address: '169 S 15th St, San Jose, CA 95112',
-      start_lat: 37.340170,
-      start_lng: -121.874510,
-      end_address: '665 S 10th St, San Jose, CA 95112',
-      end_lat: 37.33063,
-      end_lng: 	-121.87354,
-      time: '9:00 am',
+      _id: route.userId,
+      start_address: route.start_address,
+      start_lat: route.start_lat,
+      start_lng: route.start_lng,
+      end_address: route.end_address,
+      end_lat: route.end_lat,
+      end_lng: route.end_lng,
+      time: route.time,
+      default: route.default
     }
+    // const rider = {
+    //   id: "63d36e02cd478f26557c4a36",
+    //   start_address: '169 S 15th St, San Jose, CA 95112',
+    //   start_lat: 37.340170,
+    //   start_lng: -121.874510,
+    //   end_address: '665 S 10th St, San Jose, CA 95112',
+    //   end_lat: 37.33063,
+    //   end_lng: 	-121.87354,
+    //   time: '9:00 am',
+    // }
     return axios.post('/driver-list', rider)
       .then((res) => {
         console.log(res.data)
@@ -39,15 +44,7 @@ const DriverList = (props) => {
   }
 
   useEffect(() => {findDrivers()}, [])
-  useEffect(() => {findDrivers()}, [props])
-
-  const bookDriver = (driverInfo) => {
-    setBookedDriver(driverInfo)
-  }
-
-  useEffect(() => {
-    console.log(bookedDriver);
-  }, [bookedDriver]);
+  useEffect(() => {findDrivers()}, [route])
 
 
   if (drivers.length > 0) {
@@ -59,15 +56,17 @@ const DriverList = (props) => {
             driverInfo={driver.driverInfo}
             startDistance={driver.startDistance}
             endDistance={driver.endDistance}
-            bookDriver={bookDriver}
+            updateRiderOnGoingRoute={props.updateRiderOnGoingRoute}
           />
         ))}
       </div>
     )
   } else {
-    return (<div></div>)
+    return (<div className='loading-screen'>
+      <img className='loading-gif' src="https://media.tenor.com/k-wL_qZAELgAAAAi/test.gif" alt="Loading" />
+      <p>Please wait...</p>
+    </div>)
   }
-
 }
 
 export default DriverList;
