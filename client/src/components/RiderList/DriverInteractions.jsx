a5f4250943ef8753804f4c30b7c25e929b4c6a58
Nick Cassano <romanlaughs@gmail.com>
2023-02-04 12:11:58 -0800

    Hopefully Fixed!!!!!!

--- client/src/components/RiderList/DriverInteractions.jsx
+++ client/src/components/RiderList/DriverInteractions.jsx
@@ -59,34 +59,33 @@ const DriverInteractions = function(props) {
   const [driverData, setDriver] = React.useState({});
   const [time, setSeconds] = React.useState(0);
 
-
-  useEffect(() => {
-    const findRiders = () => {
-      const driver = {
-        userId: route.id,
-        start_address: route.start_address,
-        start_lat: route.start_lat,
-        start_lng: route.start_lng,
-        end_address: route.end_address,
-        end_lat: route.end_lat,
-        end_lng: route.end_lng,
-        time: route.time,
-        total_seats: route.total_seats,
-        default: route.default,
-      }
-
-      setDriver(driver);
-
-      setUserRouteInfo(driver);
-      return axios.post('/rider-list', driver)
-        .then((res) => {
-          setSeating(res.data.seats);
-          return setRiders(res.data.riders);
-        })
-        .catch((err) => console.log('Find drivers error: ', err))
+useEffect(() => {
+  const findRiders = () => {
+    const driver = {
+      userId: route.id,
+      start_address: route.start_address,
+      start_lat: route.start_lat,
+      start_lng: route.start_lng,
+      end_address: route.end_address,
+      end_lat: route.end_lat,
+      end_lng: route.end_lng,
+      time: route.time,
+      total_seats: route.total_seats,
+      default: route.default,
     }
-    findRiders();
-  }, [route])
+
+    setDriver(driver);
+
+    setUserRouteInfo(driver);
+    return axios.post('/rider-list', driver)
+      .then((res) => {
+        setSeating(res.data.seats);
+        return setRiders(res.data.riders);
+      })
+      .catch((err) => console.log('Find drivers error: ', err))
+  }
+  findRiders();
+}, [route])
 
 
   useEffect(() => {
