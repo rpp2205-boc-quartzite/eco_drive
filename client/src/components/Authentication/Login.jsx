672ffb9954a4b55305bc0d9b49ffd117f92f7250
Blake Crawford <blake.crawford@outlook.com>
2023-02-10 00:52:51 -0800

    login flow now notifies user of failed login without using alert

--- client/src/components/Authentication/Login.jsx
+++ client/src/components/Authentication/Login.jsx
@@ -4,11 +4,22 @@ import { Link } from 'react-router-dom';
 export default function Login(props) {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
+    const [emailCheck, setEmailCheck] = useState(false);
+    const [passCheck, setPassCheck] = useState(false);
 
     const handleSubmit = (event) => {
       event.preventDefault();
-      props.authCheck(email, pass);
-    }
+      if (email === '') {
+        setEmailCheck(true);
+      };
+      if (pass === '') {
+        setPassCheck(true);
+      };
+      if (pass !== '' && email !== '') {
+        props.authCheck(email, pass);
+      };
+      return;
+    };
 
     return (
         <div className='auth-form-container'>
@@ -20,15 +31,37 @@ export default function Login(props) {
                   <div className='label-title-container'>
                     <label className='label-title-3' htmlFor='email'>Email</label>
                     <div className='valid-check'>*</div>
+                    {emailCheck === true &&
+                      <div className='error-message'><span className='error-text'>Please input your email</span></div>}
                   </div>
-                  <input className='input-field' value={email} onChange={(event) => setEmail(event.target.value.toLowerCase())}type='email' id='email' name='email' />
+                  <input 
+                    className='input-field' 
+                    value={email} 
+                    onChange={(event) => {
+                      setEmail(event.target.value.toLowerCase());
+                      setEmailCheck(false);
+                      }}
+                      type='email' 
+                      id='email' 
+                      name='email' />
                 </div>
                 <div className='label-container-2'>
                   <div className='label-title-container'>
                     <label className='label-title-5' htmlFor='password'>Password</label>
                     <div className='valid-check'>*</div>
+                    {passCheck === true &&
+                      <div className='error-message'><span className='error-text'>Please input your password</span></div>}
                   </div>
-                  <input className='input-field' value={pass} onChange={(event) => setPass(event.target.value)} type='password' id='password' name='password' />
+                  <input 
+                    className='input-field' 
+                    value={pass} 
+                    onChange={(event) => {
+                      setPass(event.target.value);
+                      setPassCheck(false);
+                      }} 
+                      type='password' 
+                      id='password' 
+                      name='password' />
                 </div>
                 <div className='forgot-btn'>
                   <Link to='/password-reset'>
@@ -36,6 +69,8 @@ export default function Login(props) {
                   </Link>
                 </div>
               </div>
+              {props.loginFailure === true && 
+                <div className='login-error-message'>Incorrect email or password. Try again.</div>}
             </form>
           </div>
           <div className='link-frame'>
